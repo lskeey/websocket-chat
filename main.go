3f20c08459ca68719a2d00b325afacd0efc09dd9
lskeey <muhammd.alviansyah@gmail.com>
2025-08-20 11:29:14 +0700

    feat: Configure CORS middleware
    Adds a CORS middleware to handle cross-origin requests.
    This configuration allows:
    - GET, POST, PUT, PATCH, DELETE, and OPTIONS methods.
    - Requests from http://localhost:3000.
    - Headers: Origin, Content-Type, Authorization.
    - Exposes Content-Length header.
    - Credentials (cookies, auth headers) are allowed.
    - Preflight requests are cached for 12 hours.

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"os"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/lskeey/websocket-chat/config"
 	"github.com/lskeey/websocket-chat/handlers"
@@ -30,6 +31,16 @@ func main() {
 	go hub.Run()
 
 	router := gin.Default()
+
+	config := cors.DefaultConfig()
+  config.AllowOrigins = []string{"http://localhost:3000"}
+  config.AllowMethods = []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"}
+  config.AllowHeaders = []string{"Origin", "Content-Type", "Authorization"}
+  config.ExposeHeaders = []string{"Content-Length"}
+  config.AllowCredentials = true
+  config.MaxAge = 12 * 3600
+
+  router.Use(cors.New(config))
 
 	authHandler := &handlers.AuthHandler{DB: DB}
 	userHandler := &handlers.UserHandler{DB: DB}
